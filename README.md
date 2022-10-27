51757a56e7fd5d899b17b46832b86f4da70fc1cc
Edward Vella <hello@dwardu.com>
2022-10-27 21:39:21 +0100

    feat(app): Adding application to appdaemon.

---
+++ README.md
@@ -0,0 +1,61 @@
+# Hive TRV Boost Mode
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Default-orange.svg)](https://github.com/custom-components/hacs)
+
+*[AppDaemon](https://github.com/home-assistant/appdaemon) app to mimick the behaviour of boost mode on your Hive Radiator Valves.*
+
+In order to keep heating to a minimum, installing smart radiator valves is the first bit, however you can trigger heating for specific rooms depending on the temperatures set for your radiator valves. This will also stop heating once the target temperature is reached.
+
+This assumes you are using Zigbee2MQTT to connect to your Hive Heating thermostat.
+
+## Devices used
+
+- [Hive Thermostat SLR1b](https://www.zigbee2mqtt.io/devices/SLR1b.html)
+- [Hive UK7004240 Radiator Valve](https://www.zigbee2mqtt.io/devices/UK7004240.html)
+
+## Installation
+
+Use [HACS](https://github.com/custom-components/hacs) or [download](https://github.com/dwardu89/hive-trv-appdaemon/releases) the `heating` directory from inside the `apps` directory here to your local `apps` directory, then add the configuration to enable the `heating` module.
+
+## App configuration
+
+```yaml
+heating:
+  module: heating
+  class: HiveHeating
+  main_thermostat: climate.living_room_thermostat
+  main_thermostat_zigbee_set_topic: "zigbee2mqtt/Living Room Thermostat/set"
+  trv_list:
+    - climate.office_radiator_valve
+    - climate.main_bedroom_cove_radiator_valve
+    - climate.main_bedroom_radiator_valve
+    - climate.hallway_radiator_valve
+    - climate.second_bedroom_radiator_valve
+  log: heat_log
+  frost_protection: True
+  trv_boost_mode: input_boolean.trv_heating_boost_mode
+  trv_boost_mode_temperature: input_number.trv_boost_mode_temperature
+
+```
+
+key | optional | type | default | description
+-- | -- | -- | -- | --
+`module` | False | string | | The module name of the app.
+`class` | False | string | | The name of the Class.
+`main_thermostat` | False | string | | The entity in home assistant referring to your main Hive thermostat.
+`main_thermostat_zigbee_set_topic` | False | string | | The MQTT set topic referring to your main Hive thermostat.
+`trv_list` | False | list(string) | | The list of TRV entities which you would like to have the app monitor for boost mode.
+`log` | True | string | `heat_log` | The log in appdaemon to write the logs to. It's suggested you create a log specific to this to separate logging.
+`frost_protection` | False | boolean | True | Enables frost protection, kicking off boost mode irreespective if boost mode is enabled.
+`trv_boost_mode` | False | string | | The input_boolean entity to control this app, enabling or disabling boost mode.
+`trv_boost_mode_temperature` | False | string | | The input_number entity to determine what is the maximum boost mode value the `main_thermostat` should be set at.
+
+## Appdaemon yaml 
+
+In order to set the logs output, add this log entry to your `appdaemon.yaml` file. If you do not add it, it will default to the default log output.
+
+```yaml
+logs:
+  heat_log:
+    name: HeatLog
+```
---
+++ apps/heating.py
@@ -0,0 +1,201 @@
+import appdaemon.plugins.hass.hassapi as hass
+import json
+import datetime
+
+#
+# HiveHeating App
+#
+# Args:
+#
+
+# Will need to have something check periodically if heat is still required when nothing changes and renew the emergency_heating
+
+APP_NAME = "Hive TRV Boost Mode"
+APP_ICON = "🔥"
+
+class HiveHeating(hass.Hass):
+
+    def initialize(self):
+        self.log("Initializing Hive Heating")
+        # Trigger on change of target temperature
+        self.log("Get Main Thermostat")
+        self.log("Main Thermastat [{}]".format(self.args["main_thermostat"]))
+        self.main_thermostat = self.args["main_thermostat"]
+        self.log("Gather TRV list")
+        for entity in self.args["trv_list"]:
+            self.log("TRV [{}]".format(entity))
+        self.trv_list = self.args["trv_list"]
+        self.log("Is anyone home? [{}]".format(self.anyone_home()))
+        self.log("Registering all TRVs with a HeatRequired callback".format(entity))
+        self.listen_state(self.heating_required,
+                          self.args["trv_list"], attribute="heat_required")
+        self.log("Using [{}] to track TRV Boost mode".format(
+            self.args["trv_boost_mode"]))
+        self.trv_boost_mode_entity = self.args["trv_boost_mode"]
+        self.listen_state(self.boost_mode_disabled,
+                          self.args["trv_boost_mode"])
+        self.trv_boost_mode_temperature_entity = self.args["trv_boost_mode_temperature"]
+        self.log("Target Mode Temperature Entity [{}]".format(
+            self.trv_boost_mode_temperature_entity))
+        self.log("Testing if entity is obtainable [{}]".format(
+            self.get_state(self.trv_boost_mode_temperature_entity)), level="DEBUG")
+        self.main_thermostat_zigbee_set_topic = self.args["main_thermostat_zigbee_set_topic"]
+
+        self.SETPOINT_HOLD_DURATION = 30
+        # Check if temperatures need to be run, this is used to check if appdaemon is restarted haphazardly
+        self.run_minutely(self.temperature_check, datetime.time(0, 0, 0))
+
+    def boost_mode_disabled(self, entity, attribute, old, new, kwargs):
+        self.log("Boost Mode changed from [{}] => [{}]".format(old, new))
+
+    def heating_required(self, entity, attribute, old, new, kwargs):
+        target_temperature = self.get_state(entity, attribute="temperature")
+        current_temperature = self.get_state(
+            entity, attribute="current_temperature")
+
+        trv_boost_mode_temperature = float(
+            self.get_state(self.trv_boost_mode_temperature_entity))
+        self.log("TRV Boost Mode Temperature [{}]".format(
+            trv_boost_mode_temperature), level="DEBUG")
+
+        self.log("TRV [{}]".format(entity), level="DEBUG")
+        self.log("Attribute [{}] from {} => {}".format(
+            attribute, old, new), level="DEBUG")
+        self.log("Temperature Currently [{}], Required [{}]".format(
+            current_temperature, target_temperature), level="DEBUG")
+
+        # Check if the valve requires heat, if so enable boost mode for an hour
+        require_boost_mode = self.is_boost_mode_still_required(entity, new)
+        if self.is_boost_mode_enabled() and require_boost_mode:
+            self.log("Enabling Boost Mode")
+            # Check if there's another TRV with a higher entity boost mode
+            if target_temperature > trv_boost_mode_temperature:
+                self.set_state(self.trv_boost_mode_temperature_entity,
+                               state=target_temperature)
+                trv_boost_mode_temperature = target_temperature
+                # Also set the target temperature
+                self.start_emergency_boost(trv_boost_mode_temperature)
+        else:
+            # disable boost mode, but check if the temperatures around the other valves are required
+            # and check if those valves have boost mode enabled for them.
+            # Eventually check if the valve is in the schedule
+            self.set_state(self.trv_boost_mode_temperature_entity, state=5)
+            self.log(
+                "Disabling Boost Mode, Returning Main Thermostat to previous state")
+            self.stop_emergency_boost(5)
+
+    def is_boost_mode_still_required(self, entity, radiator_heating_required):
+        if not radiator_heating_required:
+            # If No heating is required for this trv, check if it is required for the other TRVs.
+            for trv in self.trv_list:
+                heat_required = self.get_state(trv, attribute="heat_required")
+                if heat_required:
+                    self.log("TRV [{}] still has heat_required: {}".format(
+                        trv, heat_required))
+                    return True
+        else:
+            self.log("TRV [{}] sent a heat_required: {} status".format(
+                entity, radiator_heating_required), level="DEBUG")
+            return True
+        self.log("No Heat is required any more", level="DEBUG")
+        return False
+
+    def get_max_radiator_temperature(self):
+        max_temp = 5.0
+        for trv in self.trv_list:
+            trv_temp_required = float(
+                self.get_state(trv, attribute="temperature"))
+            if trv_temp_required > max_temp:
+                max_temp = trv_temp_required
+        return max_temp
+
+    def temperature_check(self, kwargs):
+        # Check if temperatures need to be run, this is used to check if appdaemon is restarted haphazardly
+        self.log("Checking Temperature heat is required", level="DEBUG")
+
+        # Check if the valve requires heat, if so enable boost mode for an hour
+        require_boost_mode = self.is_boost_mode_still_required(
+            entity=None, radiator_heating_required=False)
+        main_thermostat_system_mode = self.get_state(
+            self.main_thermostat, attribute="system_mode")
+        self.log("Main Thermostat System Mode [{}]".format(
+            main_thermostat_system_mode), level="DEBUG")
+        if self.is_boost_mode_enabled() and require_boost_mode and not require_boost_mode == "emergency_heating":
+            self.log("Enabling Boost Mode")
+            # Get the highest temperature and start emergency boost
+            self.start_emergency_boost(self.get_max_radiator_temperature())
+        else:
+            # disable boost mode, but check if the temperatures around the other valves are required
+            # and check if those valves have boost mode enabled for them.
+            # Eventually check if the valve is in the schedule
+            # Only turn off if emergency heating is turned on. Otherwise there might be an external factor or heating.
+            if main_thermostat_system_mode == "emergency_heating" and not self.is_boost_mode_enabled():
+                self.set_state(self.trv_boost_mode_temperature_entity, state=5)
+                self.log(
+                    "Still on emergency_heating. Disabling Boost Mode")
+                self.stop_emergency_boost(5)
+
+    def generate_mqtt_message(self, system_mode, temperature_setpoint_hold_duration, temperature_setpoint_hold, occupied_heating_setpoint):
+        mqtt_message = dict()
+        mqtt_message["system_mode"] = system_mode
+        mqtt_message["temperature_setpoint_hold_duration"] = temperature_setpoint_hold_duration
+        if temperature_setpoint_hold is not None:
+            mqtt_message["temperature_setpoint_hold"] = temperature_setpoint_hold
+        mqtt_message["occupied_heating_setpoint"] = occupied_heating_setpoint
+        return json.dumps(mqtt_message)
+
+    def call_mqtt_service(self, topic, payload):
+        self.log("Publish message to topic [{}] with payload [{}]".format(
+            topic, payload), level="DEBUG")
+        self.call_service("mqtt/publish", topic=topic, payload=payload)
+
+    def ensure_target_temperature_in_range(self, target_temperature):
+        if target_temperature < 5:
+            return 5
+        elif target_temperature > 32:
+            return 32
+        return target_temperature
+
+    def start_emergency_boost(self, target_temperature):
+        target_temperature_ranged = self.ensure_target_temperature_in_range(
+            target_temperature)
+        mqtt_message = self.generate_mqtt_message(system_mode="emergency_heating",
+                                                  temperature_setpoint_hold="1",
+                                                  temperature_setpoint_hold_duration=self.SETPOINT_HOLD_DURATION,
+                                                  occupied_heating_setpoint=str(target_temperature_ranged))
+
+        self.log("Emergency Boost Mqtt message [{}]".format(
+            mqtt_message), level="DEBUG")
+
+        self.call_mqtt_service(
+            topic=self.main_thermostat_zigbee_set_topic, payload=mqtt_message)
+
+    def stop_emergency_boost(self, target_temperature):
+        target_temperature_ranged = self.ensure_target_temperature_in_range(
+            target_temperature)
+        mqtt_message = self.generate_mqtt_message(system_mode="emergency_heating",
+                                                  temperature_setpoint_hold="1",
+                                                  temperature_setpoint_hold_duration=0,
+                                                  occupied_heating_setpoint=str(target_temperature_ranged))
+
+        self.call_mqtt_service(
+            topic=self.main_thermostat_zigbee_set_topic, payload=mqtt_message)
+
+    def heating_mode_on(self, target_temperature):
+        target_temperature = ensure_target_temperature_in_range(
+            target_temperature)
+        mqtt_message = self.generate_mqtt_message(system_mode="heat",
+                                                  temperature_setpoint_hold="1",
+                                                  occupied_heating_setpoint=target_temperature)
+
+    def heating_mode_off(self, target_temperature):
+        target_temperature = ensure_target_temperature_in_range(
+            target_temperature)
+        mqtt_message = self.generate_mqtt_message(system_mode="off",
+                                                  temperature_setpoint_hold=0)
+
+    def is_boost_mode_enabled(self):
+        boost_mode_state = self.get_state(self.trv_boost_mode_entity)
+        self.log("Boost Mode State [{}]".format(
+            boost_mode_state), level="DEBUG")
+        return boost_mode_state == "on"
